f94e6eeb6fb85d485fb8d98a043d1de5f5c52d4f
elywelly <yongelynn@gmail.com>
2023-10-08 20:55:37 +1100

    update package and add func to retrieve blockchain data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,73 @@
 // ABIs
-import React, { useEffect, useState } from "react";
-// import TECK from "./abis/TECK.json";
-// // Config
-// import config from "./config.json";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import Teck from "../src/blockchain/abis/Teck.json";
+// Config
+import config from "../src/blockchain/config.json";
+
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+interface Contract {
+  [key: number]: { Teck: { address: string } };
+}
 
 function App() {
   const [message, setMessage] = useState("");
+  const [provider, setProvider] =
+    useState<ethers.providers.Web3Provider | null>(null);
+  const [account, setAccount] = useState<string | null>(null);
+  const [teck, setTeck] = useState<ethers.Contract | null>(null);
+  const [occasions, setOccasions] = useState<any[]>([]);
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    const address = (config as any)[network.chainId].Teck.address;
+    const teck = new ethers.Contract(address, Teck, provider);
+    setTeck(teck);
+
+    const totalOccasions = await teck.totalOccasions();
+    const occasions = [];
+
+    for (var i = 1; i <= totalOccasions; i++) {
+      const occasion = await teck.getOccasion(i);
+      occasions.push(occasion);
+    }
+
+    setOccasions(occasions);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
 
   useEffect(() => {
     fetch("/hi")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
+    loadBlockchainData();
   }, []);
 
-  return <div>{message}</div>;
+  return (
+    <>
+      <div>{message}</div>
+      <div>{account}</div>
+      {occasions.map((occasion, index) => {
+        return <div>{occasion.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default App;

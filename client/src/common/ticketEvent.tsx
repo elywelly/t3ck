058855373c26df459b544cb23e4896d7279cdf6f
elywelly <yongelynn@gmail.com>
2024-02-07 21:46:01 +1100

    add ticketEvent data and poster images

---
+++ client/src/common/ticketEvent.tsx
@@ -0,0 +1,67 @@
+export const ticketEvent = [
+  {
+    name: "Lauv",
+    image:
+      "https://github.com/elywelly/t3ck/blob/main/assets/posters/lauv-poster.webp",
+    description:
+      "Multi-platinum certified singer, songwriter, producer, and pop visionary Lauv has announced his return to Australia, with The Between Albums Tour landing down under this October.",
+    cost: 0.25,
+    date: "21 October 2023",
+    time: "7:00PM AEST",
+    location: "Enmore Theatre, Sydney",
+    tickets: 50,
+    category: "music",
+  },
+  {
+    name: "Chet Faker",
+    image:
+      "https://github.com/elywelly/t3ck/blob/main/assets/posters/chet-poster.webp",
+    description:
+      "Australia’s own Chet Faker will make his return down under this October! These are the first Chet Faker shows in Australia since 2015, when he performed to a sold-out Sydney Opera House forecourt crowd and played a ground-breaking five hometown shows at the Palais Theatre in Melbourne.",
+    cost: 1,
+    date: "10 October 2023",
+    time: "8:00PM AEST",
+    location: "Enmore Theatre, Sydney",
+    tickets: 80,
+    category: "music",
+  },
+  {
+    name: "Charlie Puth",
+    image:
+      "https://github.com/elywelly/t3ck/blob/main/assets/posters/charlie-poster.jpeg",
+    description:
+      "Charlie Puth has proven to be one of the industry’s most consistent hitmakers and sought-after collaborators. Puth has amassed eight multi-platinum singles, four GRAMMY nominations, three Billboard Music Awards, a Critic’s Choice Award, and a Golden Globe nomination. His 2018 GRAMMY-nominated LP, Voicenotes, was RIAA Certified Gold only four days after its release and has logged over 5.6 billion streams worldwide.",
+    cost: 2.35,
+    date: "1 November 2023",
+    time: "6:30PM AEST",
+    location: "Aware Super Theatre, Sydney",
+    tickets: 150,
+    category: "music",
+  },
+  {
+    name: "Wicked",
+    image:
+      "https://github.com/elywelly/t3ck/blob/main/assets/posters/wicked-poster.jpeg",
+    description:
+      "The Broadway sensation WICKED is flying into the Regent Theatre from March. WICKED looks at what happened in the Land of Oz... but from a different angle. Winner of over 100 major awards including the Grammy Award and three Tony Awards, WICKED is “A COMPLETE TRIUMPH! An original musical that will make you laugh, cry and think.” USA Today.",
+    cost: 5,
+    date: "8 November 2023",
+    time: "1:00PM AEST",
+    location: "Sydney Lyric Theatre, Sydney",
+    tickets: 200,
+    category: "show",
+  },
+  {
+    name: "Beauty and the Beast",
+    image:
+      "https://github.com/elywelly/t3ck/blob/main/assets/posters/beauty-poster.jpg",
+    description:
+      "Beauty and the Beast is a Disney stage musical with music by Alan Menken, lyrics by Howard Ashman and Tim Rice, and a book by Linda Woolverton.",
+    cost: 4.25,
+    date: "20 December 2023",
+    time: "1:30PM AEST",
+    location: "Capitol Theatre, Sydney",
+    tickets: 250,
+    category: "show",
+  },
+];
--- client/src/components/SearchField/SearchField.tsx
+++ client/src/components/SearchField/SearchField.tsx
@@ -1,9 +1,23 @@
-import { Autocomplete, Box, CircularProgress, TextField, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Fragment, useEffect, useState } from "react";
+import { ticketEvent } from "../../common/ticketEvent";
 
-interface Film {
-  title: string;
-  year: number;
+interface TicketEvent {
+  name: string;
+  image: string;
+  description: string;
+  cost: number;
+  date: string;
+  time: string;
+  location: string;
+  tickets: number;
+  category: string;
 }
 
 function sleep(duration: number): Promise<void> {
@@ -14,62 +28,9 @@ function sleep(duration: number): Promise<void> {
   });
 }
 
-// Top films as rated by IMDb users. http://www.imdb.com/chart/top
-const topFilms = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-];
-
-
 const SearchField = () => {
-
   const [open, setOpen] = useState(false);
-  const [options, setOptions] = useState<readonly Film[]>([]);
+  const [options, setOptions] = useState<readonly TicketEvent[]>([]);
   const loading = open && options.length === 0;
 
   useEffect(() => {
@@ -83,7 +44,7 @@ const SearchField = () => {
       await sleep(1e3); // For demo purposes.
 
       if (active) {
-        setOptions([...topFilms]);
+        setOptions([...ticketEvent]);
       }
     })();
 
@@ -92,47 +53,67 @@ const SearchField = () => {
     };
   }, [loading]);
 
-useEffect(() => {
+  useEffect(() => {
     if (!open) {
       setOptions([]);
     }
   }, [open]);
 
   return (
-    <Box sx={{ backgroundColor: '#f3f6fa', margin: '10px 24px', padding: '0 50px', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '300px', borderRadius: '5px', flexDirection: 'column'}}>
-      <Typography textAlign="center" sx={{ padding: '20px', fontWeight: 'bold', color: '#47A1FF'}}>Leveraging the power of blockchain and NFT technology to offer a secure, transparent, and seamless ticketing experience</Typography>
+    <Box
+      sx={{
+        backgroundColor: "#f3f6fa",
+        margin: "10px 24px",
+        padding: "0 50px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "300px",
+        borderRadius: "5px",
+        flexDirection: "column",
+      }}
+    >
+      <Typography
+        textAlign="center"
+        sx={{ padding: "20px", fontWeight: "bold", color: "#47A1FF" }}
+      >
+        Leveraging the power of blockchain and NFT technology to offer a secure,
+        transparent, and seamless ticketing experience
+      </Typography>
       <Autocomplete
-      id="autocomplete"
-      sx={{ width: { xs: 200, sm: 400, md: 600 } }}
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      isOptionEqualToValue={(option, value) => option.title === value.title}
-      getOptionLabel={(option) => option.title}
-      options={options}
-      loading={loading}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search for tickets"
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </Fragment>
-            ),
-          }}
-        />
-      )}
-    />
+        id="autocomplete"
+        sx={{ width: { xs: 200, sm: 400, md: 600 } }}
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        getOptionLabel={(option) => option.name}
+        options={options}
+        loading={loading}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search for tickets"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <Fragment>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </Fragment>
+              ),
+            }}
+          />
+        )}
+      />
     </Box>
-    )
-}
+  );
+};
 
 export default SearchField;

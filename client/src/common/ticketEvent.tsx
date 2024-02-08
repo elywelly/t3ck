351d75849a37407dc976eda8eb31a3b1c8d7a300
elywelly <yongelynn@gmail.com>
2024-02-08 20:32:46 +1100

    update image imports

--- client/src/common/ticketEvent.tsx
+++ client/src/common/ticketEvent.tsx
@@ -1,3 +1,9 @@
+import lauvPoster from "../posters/lauv-poster.webp";
+import chetFakerPoster from "../posters/chet-poster.webp";
+import charliePuthPoster from "../posters/charlie-poster.jpeg";
+import wickedPoster from "../posters/wicked-poster.jpeg";
+import beautyBeastPoster from "../posters/beauty-poster.jpg";
+
 export interface TicketEvent {
   name: string;
   image: string;
@@ -13,8 +19,7 @@ export interface TicketEvent {
 export const ticketEvent = [
   {
     name: "Lauv",
-    image:
-      "https://github.com/elywelly/t3ck/blob/main/assets/posters/lauv-poster.webp",
+    image: lauvPoster,
     description:
       "Multi-platinum certified singer, songwriter, producer, and pop visionary Lauv has announced his return to Australia, with The Between Albums Tour landing down under this October.",
     cost: 0.25,
@@ -26,8 +31,7 @@ export const ticketEvent = [
   },
   {
     name: "Chet Faker",
-    image:
-      "https://github.com/elywelly/t3ck/blob/main/assets/posters/chet-poster.webp",
+    image: chetFakerPoster,
     description:
       "Australia’s own Chet Faker will make his return down under this October! These are the first Chet Faker shows in Australia since 2015, when he performed to a sold-out Sydney Opera House forecourt crowd and played a ground-breaking five hometown shows at the Palais Theatre in Melbourne.",
     cost: 1,
@@ -39,8 +43,7 @@ export const ticketEvent = [
   },
   {
     name: "Charlie Puth",
-    image:
-      "https://github.com/elywelly/t3ck/blob/main/assets/posters/charlie-poster.jpeg",
+    image: charliePuthPoster,
     description:
       "Charlie Puth has proven to be one of the industry’s most consistent hitmakers and sought-after collaborators. Puth has amassed eight multi-platinum singles, four GRAMMY nominations, three Billboard Music Awards, a Critic’s Choice Award, and a Golden Globe nomination. His 2018 GRAMMY-nominated LP, Voicenotes, was RIAA Certified Gold only four days after its release and has logged over 5.6 billion streams worldwide.",
     cost: 2.35,
@@ -52,8 +55,7 @@ export const ticketEvent = [
   },
   {
     name: "Wicked",
-    image:
-      "https://github.com/elywelly/t3ck/blob/main/assets/posters/wicked-poster.jpeg",
+    image: wickedPoster,
     description:
       "The Broadway sensation WICKED is flying into the Regent Theatre from March. WICKED looks at what happened in the Land of Oz... but from a different angle. Winner of over 100 major awards including the Grammy Award and three Tony Awards, WICKED is “A COMPLETE TRIUMPH! An original musical that will make you laugh, cry and think.” USA Today.",
     cost: 5,
@@ -65,8 +67,7 @@ export const ticketEvent = [
   },
   {
     name: "Beauty and the Beast",
-    image:
-      "https://github.com/elywelly/t3ck/blob/main/assets/posters/beauty-poster.jpg",
+    image: beautyBeastPoster,
     description:
       "Beauty and the Beast is a Disney stage musical with music by Alan Menken, lyrics by Howard Ashman and Tim Rice, and a book by Linda Woolverton.",
     cost: 4.25,
---
+++ client/src/typings/index.d.ts
@@ -0,0 +1,14 @@
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
